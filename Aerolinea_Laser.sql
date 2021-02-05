7a0b60346fa3099f517ee4cca434be11f6b1c983
Estefania Cerenic <estefania.cerenic@gmail.com>
2021-02-05 11:21:31 -0300

    base de datos

--- Aerolinea_Laser.sql
+++ Aerolinea_Laser.sql
@@ -2,10 +2,23 @@ create database Aerolinea_Laser;
 use Aerolinea_Laser;
 
 create table Usuario(
-idUsuario int primary key auto_increment,
-nombreUsuario varchar(20) not null,
+id_usuario int primary key auto_increment,
+nombre_usuario varchar(20) not null,
 contrasena varchar(20) not null
 );
 
-insert into persona(nombreUsuario,contrasena) values
-(Carlos,123);
+describe Usuario;
+
+
+select * from usuario;
+
+insert into usuario(nombre_usuario,contrasena) values
+("admin","123");
+
+insert into usuario(nombre_usuario,contrasena) values
+("usertur","123");
+
+insert into usuario(nombre_usuario,contrasena) values
+("userpre","123");
+
+delete from usuario where id_usuario=2;
