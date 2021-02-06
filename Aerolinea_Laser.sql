856880ece4f3491200b39823bc686afe87d70073
Pablo <duran.pablo.c@gmail.com>
2021-02-06 16:43:15 -0300

    Modelos de datos

--- Aerolinea_Laser.sql
+++ Aerolinea_Laser.sql
@@ -1,24 +1,32 @@
-create database Aerolinea_Laser;
-use Aerolinea_Laser;
-
-create table Usuario(
-id_usuario int primary key auto_increment,
-nombre_usuario varchar(20) not null,
-contrasena varchar(20) not null
+DROP DATABASE IF EXISTS Aerolinea_Laser;
+CREATE DATABASE Aerolinea_Laser;
+USE Aerolinea_Laser;
+CREATE TABLE Tipo_Usuario(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(20) NOT NULL
 );
+CREATE TABLE Usuarios(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(20) NOT NULL,
+    apellido VARCHAR(20) NOT NULL,
+    correo VARCHAR(40) NOT NULL,
+    usuario VARCHAR(20) NOT NULL,
+    contrasena VARCHAR(20) NOT NULL,
+    tipo_usuario_id INT NOT NULL,
+    CONSTRAINT fk_tipo_usuario_id FOREIGN KEY (tipo_usuario_id) REFERENCES Tipo_Usuario (id)
+);
+INSERT INTO Tipo_Usuario(nombre)
+VALUES ("admin");
+INSERT INTO Tipo_Usuario(nombre)
+VALUES ("turismo");
+INSERT INTO Tipo_Usuario(nombre)
+VALUES ("premium");
+INSERT INTO Usuarios(nombre, apellido, correo, usuario, contrasena, tipo_usuario_id)
+VALUES ("Administrador", "Uno", "admin@laser.com", "admin", "123", 1);
+INSERT INTO Usuarios(nombre, apellido, correo, usuario, contrasena, tipo_usuario_id)
+VALUES ("Usuario", "Turista", "turista@gmail.com", "usertur", "123", 2);
+INSERT INTO Usuarios(nombre, apellido, correo, usuario, contrasena, tipo_usuario_id)
+VALUES ("Usuario", "Premium", "premium@gmail.com", "userpre", "123", 3);
 
-describe Usuario;
-
-
-select * from usuario;
-
-insert into usuario(nombre_usuario,contrasena) values
-("admin","123");
-
-insert into usuario(nombre_usuario,contrasena) values
-("usertur","123");
-
-insert into usuario(nombre_usuario,contrasena) values
-("userpre","123");
-
-delete from usuario where id_usuario=2;
+SELECT *
+FROM Usuarios;
